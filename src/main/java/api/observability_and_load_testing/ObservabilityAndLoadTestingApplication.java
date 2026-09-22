ebdb80c33d39954b88089d052ac1e47787e3d6a6
Sana Gul <sanashaaah4@gmail.com>
2026-09-22 15:33:01 +0500

    first commit

---
+++ src/main/java/api/observability_and_load_testing/ObservabilityAndLoadTestingApplication.java
@@ -0,0 +1,13 @@
+package api.observability_and_load_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityAndLoadTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilityAndLoadTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/observability_and_load_testing/ObservabilityAndLoadTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package api.observability_and_load_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityAndLoadTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
